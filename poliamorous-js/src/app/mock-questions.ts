d2f262b9f9b82af5f7deb1fda5ef4f350b305ef7
Matt Robinson <mthw.wm.robinson@gmail.com>
2017-07-11 21:49:43 -0400

    created question component

---
+++ poliamorous-js/src/app/mock-questions.ts
@@ -0,0 +1,28 @@
+import { Question } from './question';
+
+export const QUESTIONS: Question[] = [
+  {
+    id: 1,
+    question: 'Who did you vote for in the 2017 Presidential election?',
+    option1: 'Clinton',
+    option2: 'Trump'
+  },
+  {
+    id: 2,
+    question: 'What party do you identify with?'
+    option1: 'Democratic',
+    option2: 'Republican'
+  },
+  {
+    id: 3,
+    question: 'In general, do you believe a lower tax burden leads to greater economic growth?',
+    option1: 'Yes',
+    option2: 'No
+  },
+  {
+    id: 4,
+    question: 'Do you believe that health care is a universal right?',
+    option1: 'Yes',
+    option2: 'No
+  }
+];
---
+++ poliamorous-js/src/app/question.ts
@@ -0,0 +1,6 @@
+export class Question {
+  id: number;
+  question: string;
+  option1: string;
+  option2: string;
+}
