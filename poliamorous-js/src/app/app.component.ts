d0ab27423444f00436c68e8931fc4a65c07a21fb
Matt Robinson <mthw.wm.robinson@gmail.com>
2017-07-15 12:44:26 -0400

    added nav buttons to tops

--- poliamorous-js/src/app/app.component.ts
+++ poliamorous-js/src/app/app.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-root',
@@ -6,5 +7,20 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
+  // Constructor
+  constructor(
+    private router: Router,
+  ){}
+
   title = 'Poliamorous';
+
+  navigate(page: string): void {
+    if(page=='questions'){
+      this.router.navigate(['/question',1])
+    }
+    else if(page=='priorities'){
+      this.router.navigate(['/priorities'])
+    }
+  }
+
 }
