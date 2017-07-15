8cc3f96dfb74239083e414c0877d82a959d3cf28
Matt Robinson <mthw.wm.robinson@gmail.com>
2017-07-15 12:52:21 -0400

    added goofy questions

---
+++ poliamorous-js/src/app/priorities/mock-priorities.ts
@@ -0,0 +1,8 @@
+export const PRIORITIES: string[] = [
+  'Intergalactic Affairs',
+  'Economy',
+  'Health Care',
+  'Foreign Policy',
+  'Lizard People',
+  'Taco Eating Contests'
+]
--- poliamorous-js/src/app/question/mock-questions.ts
+++ poliamorous-js/src/app/question/mock-questions.ts
@@ -3,6 +3,24 @@ import { Question } from './question';
 export const QUESTIONS: Question[] = [
   {
     id: 1,
+    text: 'Out of everyone at shul, who do you think can eat the most tacos? (Other than Eric, who can eat three Chipotle burritos)',
+    options: [
+      'Matt',
+      'James'
+    ],
+    choice: null
+  },
+  {
+    id: 2,
+    text: 'Out of all the Trump administration officials, who do you tink is most likely to be a reptilian?',
+    options: [
+      'Steve Bannon',
+      'Jared Kushner'
+    ],
+    choice: null
+  },
+  {
+    id: 3,
     text: 'Who did you vote for in the 2016 Presidential election?',
     options: [
       'Clinton',
@@ -11,7 +29,7 @@ export const QUESTIONS: Question[] = [
     choice: null
   },
   {
-    id: 2,
+    id: 4,
     text: 'What party do you identify with?',
     options: [
       'Democratic',
@@ -20,7 +38,7 @@ export const QUESTIONS: Question[] = [
     choice: null
   },
   {
-    id: 3,
+    id: 5,
     text: 'In general, do you believe a lower tax burden leads to greater economic growth?',
     options: [
       'Yes',
@@ -29,7 +47,7 @@ export const QUESTIONS: Question[] = [
     choice: null
   },
   {
-    id: 4,
+    id: 6,
     text: 'Do you believe that health care is a universal right?',
     options: [
       'Yes',
