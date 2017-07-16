25a11777369dc3070250e2daab1bbb9eee510ae2
Matt Robinson <mthw.wm.robinson@gmail.com>
2017-07-16 00:06:55 -0400

    added profile editor

--- poliamorous-js/src/app/profile/profile.component.ts
+++ poliamorous-js/src/app/profile/profile.component.ts
@@ -12,4 +12,20 @@ import 'rxjs/add/operator/switchMap';
   styleUrls: ['./profile.component.css']
 })
 export class ProfileComponent {
+  //Constructor
+  constructor(
+    private profileService: ProfileService,
+    private route: ActivatedRoute,
+    private router: Router,
+  ){}
+
+  @Input() user: User;
+
+  ngOnInit(): void {
+    this.route.paramMap
+      .switchMap((params: ParamMap) =>
+        this.profileService.getProfile())
+    .subscribe(user => this.user = user);
+  }
+
 }
